48e3cd4a725e4ca3f8c518f76cd5454e4b7a3f82
Brendan Hurt <brendan.q.hurt@gmail.com>
2021-10-10 18:45:47 -0700

    seems to be working for a fairly brute-force method

--- knightsTour.py
+++ knightsTour.py
@@ -9,15 +9,27 @@ currentX = 0
 currentY = 0
 currentStep = 0 # tracks the progress through the board
 notVis = -1 # value of board spaces that haven't been visited yet
+newHigh = 0 # tracker for the farthest the algorithm has gotten
 
 #set space to -1 to make checking if a spot has been visited easier
 board = [[notVis for x in range(boardSize)] for y in range(boardSize)]
 
+def printBoard():
+    for row in board:
+        print(row)
+    print(" ") #just to make a new line
 
 #helper function for recursing through the steps of the algorithm
 #   mostly to reduce the amount of repeated code, and to make it more readable
 def recurse(x, y, step):
+    global newHigh # remove later
     board[x][y] = step
+    #---------------------------------------
+    #remove this section later
+    if step > newHigh:
+        newHigh = step
+        print("new highest: ", step)
+    #---------------------------------------
     tour(x, y, step)
 
 #order of checks:
@@ -26,84 +38,66 @@ def recurse(x, y, step):
 #   up-left & up-right
 #   left-up & left-down
 def tour(curX, curY, curStep):
-    print(curStep, totalSize) #remove later
+    #print(curStep, totalSize) #remove later
     if curStep < totalSize:
         #------------------------------------
         #search down
         if curX + 2 < boardSize:
-            print(curStep, "checking down")
             #search left
             if curY - 1 >= 0:
-                print(curStep, "checking down-left")
                 if board[curX + 2][curY  - 1] == notVis:
                     recurse(curX + 2, curY - 1, curStep + 1)
 
             #search right
             if curY + 1 < boardSize:
-                print(curStep, "checking down-right")
                 if board[curX + 2][curY + 1] == notVis:
                     recurse(curX + 2, curY + 1, curStep + 1)
         #------------------------------------
         #search right
         if curY + 2 < boardSize:
-            print(curStep, "checking right")
             #search up
             if curX - 1 >= 0:
-                print(curStep, "checking right-up")
                 if board[curX - 1][curY + 2] == notVis:
                    recurse(curX - 1, curY + 2, curStep + 1)
 
             #search down
             if curX + 1 < boardSize:
-                print(curStep, "checking right-down")
                 if board[curX + 1][curY + 2] == notVis:
                     recurse(curX + 1, curY + 2, curStep + 1)
         #------------------------------------
         #search up
         if curX - 2 >= 0:
-            print(curStep, "checking up")
             #search left
             if curY - 1 >= 0:
-                print(curStep, "checking up-left")
                 if board[curX - 2][curY - 1] == notVis:
                     recurse(curX - 2, curY - 1, curStep + 1)
 
             #search right
             if curY + 1 < boardSize:
-                print(curStep, "checking up-right")
                 if board[curX - 2][curY + 1] == notVis:
                     recurse(curX - 2, curY + 1, curStep + 1)
         #------------------------------------
         #search left
         if curY - 2 >= 0:
-            print(curStep, "checking left")
             #search up
             if curX - 1 >= 0:
-                print(curStep, "checking left-up")
                 if board[curX - 1][curY - 2] == notVis:
                     recurse(curX - 1, curY - 2, curStep + 1)
 
             #search down
             if curX + 1 < boardSize:
-                print(curStep, "checking left-down")
                 if board[curX + 1][curY - 2] == notVis:
                     recurse(curX + 1, curY - 2, curStep + 1)
         #------------------------------------
         #reset the board space & recurse back up
-        #else:
-            #board[curX][curY] = notVis
+        board[curX][curY] = notVis
 
     
 
 
-
-#print empty board (probably remove later)
-for row in board:
-    print(row)
-print(" ") #just to make a new line
+printBoard()
 
 board[0][0] = 0 #change to make any start point available later?
 tour(currentX, currentY, currentStep)
 
-for row in board:
-    print(row)
+printBoard()
